a732888eb3143986654a96ea89ab77f269c72ce2
Maria-Pahome <70057309+Maria-Pahome@users.noreply.github.com>
2021-09-01 14:08:14 +0300

    Finished html/css exercice

--- resources/main.js
+++ resources/main.js
@@ -77,10 +77,10 @@ function activeCursor(e) {
   }
 
 function navToggle(e){
-  gsap.to(".line1",0.5,{rotate: "45", y: 5, background: 'red'});
-  gsap.to(".line2",0.5,{rotate: "-45", y: -5, background: 'red'});
+  gsap.to(".line1",0.5,{rotate: "45", y: 5, background: "black"});
+  gsap.to(".line2",0.5,{rotate: "-45", y: -5, background: "black"});
   gsap.to("#logo", 1, { color:"black"});
-  gsap.to(".nav-bar", 2, {clipPath: 'circle(2500px at 100% -10%)'});
+  gsap.to(".nav-bar", 2, {clipPath: "circle(2500px at 100% -10%)"});
 }
 
 
