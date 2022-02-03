e8e6568eedf58a13d189102626752d527b603f1c
Maria-Pahome <70057309+Maria-Pahome@users.noreply.github.com>
2022-02-03 20:44:12 +0200

    changes class

--- resources/main.js
+++ resources/main.js
@@ -95,41 +95,73 @@ function navToggle(e){
 }
 
 //Barba Page Transitions
-const logo = document.querySelector('#logo');
+const logo = document.querySelector("#logo");
 barba.init({
-  views: [{
-    namespace: "home",
-    beforeEnter(){
-      animateSlides();
+  views: [
+    {
+      namespace: "home",
+      beforeEnter() {
+        animateSlides();
+        logo.href = "fashion.html";
+      },
+      beforeLeave() {
+        slideScene.destroy();
+        pageScene.destroy();
+        controller.destroy();
+      }
     },
-    beforeLeave(){
-      slideScene.destroy();
-      pageScene.destroy();
-      controller.destroy();
+    {
+      namespace: "fashion",
+      beforeEnter() {
+        logo.href = "index.html";
+      },
+      beforeLeave() {
+        controller.destroy();
+        detailScene.destroy();
+      }
     }
-  }, 
-  {
-    namespace:"fashion",
-  }
-], 
+  ],
   transitions: [
     {
-      leave({current, next}){
-        let done= this.async();
-          //animations
-        window.scrollTo(0,0);
-        const tl = gsap.timeline({defaults:{ease:"power2.inOut"} });
-        tl.fromTo(current.container,1,{opacity:1},{opacity:0});
-        tl.fromTo('.swipe',0.75, {x:'-100%'},{x:'0%',onComplete:done},"-=0.5");
+      leave({ current, next }) {
+        let done = this.async();
+        //An Animation
+        const tl = gsap.timeline({ defaults: { ease: "power2.inOut" } });
+        tl.fromTo(current.container, 1, { opacity: 1 }, { opacity: 0 });
+        tl.fromTo(
+          ".swipe",
+          0.75,
+          { x: "-100%" },
+          { x: "0%", onComplete: done },
+          "-=0.5"
+        );
       },
-      enter({current, next}){
-        let done= this.async();
-        const tl = gsap.timeline({defaults:{ease:"power2.inOut"} });
-        tl.fromTo(next.container,1,{opacity:0},{opacity:1, onComplete: done});
+      enter({ current, next }) {
+        let done = this.async();
+        //Scroll to the top
+        window.scrollTo(0, 0);
+        //An Animation
+        const tl = gsap.timeline({ defaults: { ease: "power2.inOut" } });
+        tl.fromTo(
+          ".swipe",
+          1,
+          { x: "0%" },
+
+          { x: "100%", stagger: 0.6, onComplete: done }
+        );
+        tl.fromTo(next.container, 1, { opacity: 0 }, { opacity: 1 });
+        tl.fromTo(
+          ".nav-header",
+          1,
+          { y: "-100%" },
+          { y: "0%", ease: "power2.inOut" },
+          "-=1.5"
+        );
       }
-  }
-] 
+    }
+  ]
 });
+
 
 
 //Event Listeners 
